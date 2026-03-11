8a3b0c49f08f366a49f67ff8fb721556725c654f
Aaron <aaronq8@gmail.com>
2026-03-12 02:03:13 +0530

    minor stuff

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ cmake -S . -B build
 cd build
 make all
 sudo apt install libboost-all-dev
+
+## Completed Tasks
+1. GCC Driver
+
+## TODO
+1. Lexer
+2. Parser
+3. Codegen
--- include/compiler_driver.hpp
+++ include/compiler_driver.hpp
@@ -4,10 +4,17 @@
 class CompilerDriver {
 public:
   CompilerDriver(const std::string &input_file_path);
+  // disable copying and std::move
+  CompilerDriver(const CompilerDriver &) = delete;
+  CompilerDriver &operator=(const CompilerDriver &) = delete;
+  CompilerDriver(CompilerDriver &&) = delete;
+  CompilerDriver &operator=(CompilerDriver &&) = delete;
+  // main driver method
   int32_t run();
 
 private:
   int32_t preprocess();
+  // just stub response for now..
   int32_t compile();
   int32_t link();
   std::string get_file_name();
